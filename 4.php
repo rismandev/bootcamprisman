a1145f704f3d7bc63449c10b989f8a225dbb8e3c
rismandev <rismandev@gmail.com>
2019-07-20 20:47:42 +0700

    Perbaikan

--- 4.php
+++ 4.php
@@ -2,10 +2,12 @@
 
 class HitungSalaman
 {
+    // Fungsi Hitung Jumlah Satu orang Salaman
     function satuOrang($jumlahOrang)
     {
         return $satuOrangSalaman =  $jumlahOrang - 1;
     }
+    // Fungsi Hitung Jumlah Semua salaman
     function semuaOrang($jumlahSalaman, $jmlOrg)
     {
         $semuaOrangSalaman = $jumlahSalaman * $jmlOrg;
@@ -13,11 +15,13 @@ class HitungSalaman
     }
 }
 
+// Instansiasi Objek
 $hitungSalaman = new HitungSalaman();
 
 // Menentukan Jumlah orang
 $jmlOrg = 80;
 
+// Memanggil Method
 $satuOrang = $hitungSalaman->satuOrang($jmlOrg);
 $semuaOrang = $hitungSalaman->semuaOrang($satuOrang, $jmlOrg);
 
